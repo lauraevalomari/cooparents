eba85ffbc5c7a387f2b0114ebb6ea155832f61e6
Myriam ZAIBET <myriam.zaibet@gmail.com>
2024-03-23 17:33:47 +0100

    tâche disparait du dashboard une fois complétée

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,6 @@ class Task < ApplicationRecord
   belongs_to :task_creator, class_name: "User"
   belongs_to :parent_in_charge, class_name: "User"
 
-  scope :two_first_tasks_for_user, ->(current_user) { where(parent_in_charge_id: current_user.id).order(deadline: :desc).first(2) }
+  scope :two_first_tasks_for_user, ->(current_user) { where(parent_in_charge_id: current_user.id, status: false).order(deadline: :asc).first(2) }
   scope :all_tasks_for_user, ->(current_user) { where(parent_in_charge_id: current_user.id).order(deadline: :desc) }
 end
