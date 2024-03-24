ab0ac3188fb9f6e8ce066e96810160d85532d72f
Myriam ZAIBET <myriam.zaibet@gmail.com>
2024-03-24 14:38:27 +0100

    pour merge

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   belongs_to :task_creator, class_name: "User"
   belongs_to :parent_in_charge, class_name: "User"
 
+  CATEGORIES = ['Santé', 'Scolarité', 'Activités', 'Juridique', 'Autres']
+
   scope :two_first_tasks_for_user, ->(current_user) { where(parent_in_charge_id: current_user.id, status: false).order(deadline: :asc).first(2) }
   scope :all_pending_tasks_for_user, ->(current_user) { where(parent_in_charge_id: current_user.id, status: false).order(deadline: :asc) }
   scope :all_tasks_for_user, ->(current_user) { where(parent_in_charge_id: current_user.id).order(deadline: :desc) }
