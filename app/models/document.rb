167d071d5fbd085d4ec66209aebd276ef0d780eb
Laura-Eva LOMARI <laura.lomari@gmail.com>
2024-03-25 23:12:23 +0400

    icons added on document and contact forms

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,7 +3,7 @@ class Document < ApplicationRecord
   has_one_attached :attachment
   belongs_to :child, optional: true
   belongs_to :document_creator, class_name: "User"
-  enum category: { "Santé" => 0, "Scolarité" => 1, "Activités" => 2, "Juridique" => 3, "Autres" => 4 }
+  CATEGORIES = ['Santé', 'Scolarité', 'Activités', 'Juridique', 'Autres']
   enum doctype: { "Etat civil" => 0, "Assurance" => 1, "Résultats" => 2, "Bulletin scolaire" => 3, "Liste de pièces" => 4, "Ordonnance" => 5, "Résultats d'analyse/ Compte-rendu médical" => 6, "Jugement" => 7, "Sortie du territoire" => 8, "Photo" => 9, "Autre" => 10 }
   has_rich_text :rich_details
 
