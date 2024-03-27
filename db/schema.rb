cc1e3a70e5013dae00f6ee74069fc094522f9d2c
Eleonore Lacombe <eleonore.lac@gmail.com>
2024-03-27 17:41:45 +0100

    modif de l'index document et de quelques éléments de fronts

--- db/schema.rb
+++ db/schema.rb
@@ -89,12 +89,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_26_230523) do
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
-  create_table "chatrooms", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "children", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
